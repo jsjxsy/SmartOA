19d53d71a269e4ffc28748bab4952e19bcc13b2d
jsjxsy <jsjxsy@gmail.com>
2019-12-19 16:52:59 +0800

    接口问题

---
+++ SmartOA/app/src/main/java/com/gx/smart/smartoa/data/network/api/AccessRecordProviderService.java
@@ -0,0 +1,75 @@
+package com.gx.smart.smartoa.data.network.api;
+
+import com.gx.smart.smartoa.data.network.api.base.CallBack;
+import com.gx.smart.smartoa.data.network.api.base.GrpcAsyncTask;
+import com.gx.wisestone.work.grpc.ds.accessrecord.AccessRecordProviderGrpc;
+import com.gx.wisestone.work.grpc.ds.accessrecord.AccessRecordReportedDto;
+import com.gx.wisestone.work.grpc.ds.accessrecord.AccessRecordReportedResp;
+import com.orhanobut.logger.Logger;
+
+import java.util.concurrent.TimeUnit;
+
+import io.grpc.ManagedChannel;
+
+/**
+ * @author xiaosy
+ * @create 2019-12-19
+ * @Describe
+ **/
+public class AccessRecordProviderService {
+    //25秒，网络请求超时
+    public static final int TIMEOUT_NETWORK = 25;
+
+    private static AccessRecordProviderService UserCenterClient;
+
+    private AccessRecordProviderService() {
+
+    }
+
+    public static AccessRecordProviderService getInstance() {
+        if (UserCenterClient == null) {
+            UserCenterClient = new AccessRecordProviderService();
+        }
+        return UserCenterClient;
+    }
+
+    /**
+     * 获取UserCenterstub  设置超时时间  8秒
+     *
+     * @param channel
+     * @return
+     */
+    public AccessRecordProviderGrpc.AccessRecordProviderBlockingStub getAppAttendance(ManagedChannel channel) {
+        return AccessRecordProviderGrpc.newBlockingStub(channel)
+                .withDeadlineAfter(TIMEOUT_NETWORK, TimeUnit.SECONDS);
+
+    }
+
+
+    /**
+     * 广告图 所有app一样
+     *
+     * @return callBack返回值
+     */
+    public GrpcAsyncTask<String, Void, AccessRecordReportedResp> accessRecordReported(String latitude, String longitude,
+                                                                                      String address, CallBack callBack) {
+        return new GrpcAsyncTask<String, Void, AccessRecordReportedResp>(callBack) {
+            @Override
+            protected AccessRecordReportedResp doRequestData(ManagedChannel channel) {
+                AccessRecordReportedDto message = AccessRecordReportedDto.newBuilder()
+                        .setLat(latitude)
+                        .setLng(longitude)
+                        .setAddress(address)
+                        .build();
+                AccessRecordReportedResp response = null;
+                try {
+                    response = getAppAttendance(channel).accessRecordReported(message);
+                } catch (Exception e) {
+                    Logger.e("AppAttendanceService", e.getMessage());
+                }
+
+                return response;
+            }
+        }.doExecute();
+    }
+}
---
+++ SmartOA/app/src/main/java/com/gx/smart/smartoa/data/network/api/AttendanceAppProviderService.java
@@ -0,0 +1,97 @@
+package com.gx.smart.smartoa.data.network.api;
+
+import com.gx.smart.smartoa.data.network.api.base.CallBack;
+import com.gx.smart.smartoa.data.network.api.base.GrpcAsyncTask;
+import com.gx.wisestone.work.grpc.ds.attendanceapp.AttendanceAppProviderGrpc;
+import com.gx.wisestone.work.grpc.ds.attendanceapp.getEmployeeDayRecordResp;
+import com.gx.wisestone.work.grpc.ds.attendanceapp.getEmployeeRecordDto;
+import com.gx.wisestone.work.grpc.ds.attendanceapp.getEmployeeRecordListQueryReq;
+import com.orhanobut.logger.Logger;
+
+import java.util.concurrent.TimeUnit;
+
+import io.grpc.ManagedChannel;
+
+/**
+ * @author xiaosy
+ * @create 2019-12-19
+ * @Describe
+ **/
+public class AttendanceAppProviderService {  //25秒，网络请求超时
+    public static final int TIMEOUT_NETWORK = 25;
+
+    private static AttendanceAppProviderService UserCenterClient;
+
+    private AttendanceAppProviderService() {
+
+    }
+
+    public static AttendanceAppProviderService getInstance() {
+        if (UserCenterClient == null) {
+            UserCenterClient = new AttendanceAppProviderService();
+        }
+        return UserCenterClient;
+    }
+
+    /**
+     * 获取UserCenterstub  设置超时时间  8秒
+     *
+     * @param channel
+     * @return
+     */
+    public AttendanceAppProviderGrpc.AttendanceAppProviderBlockingStub getAppAttendance(ManagedChannel channel) {
+        return AttendanceAppProviderGrpc.newBlockingStub(channel)
+                .withDeadlineAfter(TIMEOUT_NETWORK, TimeUnit.SECONDS);
+
+    }
+
+
+    /**
+     * 查询员工今日考勤记录
+     *
+     * @return callBack返回值
+     */
+    public GrpcAsyncTask<String, Void, getEmployeeDayRecordResp> getEmployeeDayRecord(String latitude, String longitude,
+                                                                                      String address, CallBack callBack) {
+        return new GrpcAsyncTask<String, Void, getEmployeeDayRecordResp>(callBack) {
+            @Override
+            protected getEmployeeDayRecordResp doRequestData(ManagedChannel channel) {
+                getEmployeeRecordDto message = getEmployeeRecordDto.newBuilder()
+                        .build();
+                getEmployeeDayRecordResp response = null;
+                try {
+                    response = getAppAttendance(channel).getEmployeeDayRecord(message);
+                } catch (Exception e) {
+                    Logger.e("AppAttendanceService", e.getMessage());
+                }
+
+                return response;
+            }
+        }.doExecute();
+    }
+
+
+    /**
+     * 查询员工今日考勤记录
+     *
+     * @return callBack返回值
+     */
+    public GrpcAsyncTask<String, Void, getEmployeeDayRecordResp> getEmployeeRecordList(String latitude, String longitude,
+                                                                                       String address, CallBack callBack) {
+        return new GrpcAsyncTask<String, Void, getEmployeeDayRecordResp>(callBack) {
+            @Override
+            protected getEmployeeDayRecordResp doRequestData(ManagedChannel channel) {
+                getEmployeeRecordListQueryReq message = getEmployeeRecordListQueryReq.newBuilder()
+                        .build();
+                getEmployeeDayRecordResp response = null;
+                try {
+                    response = getAppAttendance(channel).getEmployeeRecordList(message);
+                } catch (Exception e) {
+                    Logger.e("AppAttendanceService", e.getMessage());
+                }
+
+                return response;
+            }
+        }.doExecute();
+    }
+}
